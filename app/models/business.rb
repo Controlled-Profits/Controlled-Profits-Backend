ef602e25ff7a4341d77de7deef251ba294a0fb6f
oakley <oakleypeavler@gmail.com>
2017-09-05 18:57:28 -0400

    Open up validations for blank inputs

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,4 @@
 class Business < ApplicationRecord
   belongs_to :user
-  validates_format_of :ein, with: /\A([0-9]){2}(-)([0-9]){7}\z/, message: "EIN must be given in the format: XX-XXXXXXX", 
-    if: Proc.new { :ein != "" }
+  validates_format_of :ein, with: /\A([0-9]){2}(-)([0-9]){7}\z/, message: "EIN must be given in the format: XX-XXXXXXX", allow_blank: true
 end
